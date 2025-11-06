85e362b7ffb9acefbc0ec259fcb9650e126a980e
_.mindang <minhdang875425@gmail.com>
2025-11-06 17:25:20 +0700

    change schema route_stop_students

--- frontend/src/app/driver/students/page.tsx
+++ frontend/src/app/driver/students/page.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import StudentCard from "@/components/admin/StudentCard"
+import StudentCard from "@/components/driver/StudentCard"
 
 const Page = () => {
   // Mock data=
@@ -7,10 +7,10 @@ const Page = () => {
     { id: 1, name: "Trần Thị An", stop: "Điểm Dừng Chính & Đại Lộ Sồi", time: "07:30 AM", status: "Đã Đón" },
     { id: 2, name: "Nguyễn Văn B", stop: "Trạm xe Nguyễn Trãi", time: "07:40 AM", status: "Chưa Đón" },
     { id: 3, name: "Lê Thị C", stop: "Ngã tư Hàng Xanh", time: "07:50 AM", status: "Đã Đón" },
-     { id: 3, name: "Lê Thị C", stop: "Ngã tư Hàng Xanh", time: "07:50 AM", status: "Đã Đón" },
-      { id: 3, name: "Lê Thị C", stop: "Ngã tư Hàng Xanh", time: "07:50 AM", status: "Đã Đón" },
-       { id: 3, name: "Lê Thị C", stop: "Ngã tư Hàng Xanh", time: "07:50 AM", status: "Đã Đón" },
-        { id: 3, name: "Lê Thị C", stop: "Ngã tư Hàng Xanh", time: "07:50 AM", status: "Đã Đón" },
+     { id: 4, name: "Lê Thị C", stop: "Ngã tư Hàng Xanh", time: "07:50 AM", status: "Đã Đón" },
+      { id: 5, name: "Lê Thị C", stop: "Ngã tư Hàng Xanh", time: "07:50 AM", status: "Đã Đón" },
+       { id: 6, name: "Lê Thị C", stop: "Ngã tư Hàng Xanh", time: "07:50 AM", status: "Đã Đón" },
+        { id: 7, name: "Lê Thị C", stop: "Ngã tư Hàng Xanh", time: "07:50 AM", status: "Đã Đón" },
   ]
 
   return (
@@ -22,8 +22,9 @@ const Page = () => {
         </p>
         <div className='overflow-x-auto'>
             <div className="min-w-[900px]">
-        {/* Header */}
-        <div className="grid grid-cols-[4fr_5fr_4fr_3fr_2fr] py-6 text-center text-black border-b border-gray-300 font-bold">
+
+        <div className="grid grid-cols-[3fr_2fr_4fr_2fr_2fr_1fr] py-6 text-center text-black border-b border-gray-300 font-bold">
+          <span>Ảnh Đại Diện</span>
           <span>Tên Học Sinh</span>
           <span>Tên Điểm Dừng</span>
           <span>Thời Gian</span>
@@ -31,7 +32,6 @@ const Page = () => {
           <span>Hành Động</span>
         </div>
 
-        {/* Render danh sách */}
         {students.map((student) => (
           <StudentCard key={student.id} data={student} />
         ))}
--- frontend/src/components/driver/StudentCard.tsx
+++ frontend/src/components/driver/StudentCard.tsx
@@ -1,53 +1,52 @@
 'use client'
 
 import React, {useState} from 'react';
+import Image from "next/image";
 import {
     DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
+    DropdownMenuContent, DropdownMenuItem,
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import {Ellipsis} from "lucide-react";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
+    AlertDialog, AlertDialogAction, AlertDialogCancel,
     AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
+    AlertDialogDescription, AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle
 } from "@/components/ui/alert-dialog";
 import {Dialog, DialogContent, DialogHeader, DialogTitle} from "@/components/ui/dialog";
 import StudentForm from "@/components/admin/students/StudentForm";
-import Image from "next/image";
 
 const StudentCard = ({ student, handleDelete }: any) => {
     const [open, setOpen] = useState(false)
     const [openAlert, setOpenAlert] = useState(false)
 
     return (
-        <div className='grid grid-cols-[2fr_3fr_3fr_2fr] items-center py-6 text-center text-black border-b border-gray-300'>
-            <span>{student?.users?.full_name}</span>
+        <div className='grid grid-cols-[3fr_2fr_4fr_2fr_2fr_1fr] items-center py-6 text-center text-black border-b border-gray-300'>
             <span className="w-50 h-12 relative overflow-hidden rounded-full mx-auto">
                 <Image
-                    src={student.profile_photo_url}
-                    alt={student.full_name}
+                    src={student?.profile_photo_url}
+                    alt={student?.full_name}
                     fill
                     style={{ objectFit: 'contain' }}
                 />
             </span>
-            <span>{student.full_name}</span>
+            <span>{student?.full_name}abc</span>
+            <span>tuyến đuonwgf</span>
+            <span>6:30</span>
+            <span>da don</span>
             <span>
                 <DropdownMenu>
                 <DropdownMenuTrigger><Ellipsis /></DropdownMenuTrigger>
                     <DropdownMenuContent>
                         <DropdownMenuLabel>Hành Động</DropdownMenuLabel>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem onClick={() => setOpen(true)}>Chỉnh Sửa</DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => setOpenAlert(true)}>Xóa</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setOpen(true)}>Đã đón</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setOpenAlert(true)}>Đã tới nơi</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setOpenAlert(true)}>Không xuất hiện</DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
 
